86c0d6a3c0c8b6661f960111d298e414bbb30a1c
Ashif Mujtoba <nmujtoba@gmail.com>
2022-10-10 01:17:30 +0600

    room db test cases written done

--- app/src/androidTest/java/com/demo/currencyconvert/CurrencyDaoTest.kt
+++ app/src/androidTest/java/com/demo/currencyconvert/CurrencyDaoTest.kt
@@ -5,12 +5,11 @@ import androidx.test.filters.SmallTest
 import com.demo.currencyconvert.data.room.CurrencyDAO
 import com.demo.currencyconvert.data.room.CurrencyDatabase
 import com.demo.currencyconvert.data.room.CurrencyEntity
+import com.demo.currencyconvert.data.room.LatestRateEntity
 import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -44,9 +43,8 @@ class CurrencyDaoTest {
         currencyDatabase.close()
     }
 
-    //@OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun insertUser() = runBlocking{
+    fun insertUser() = runBlocking {
         val currency = CurrencyEntity(
             currencyName = "Euro",
             currencyBalance = 1000.00
@@ -54,6 +52,54 @@ class CurrencyDaoTest {
         currencyDao.insert(currency)
         val allCurrencies = currencyDao.getAllCurrencies()
         assertThat(allCurrencies).contains(currency)
+    }
+
+    @Test
+    fun insertAndReadCurrencies() = runBlocking {
+        val currency = CurrencyEntity(
+            currencyName = "EUR",
+            currencyBalance = 1000.0
+        )
+        currencyDao.insert(currency)
+        val allCurrencies = currencyDao.getAllCurrencies()
+        assertThat(allCurrencies).contains(currency)
+    }
+
+    @Test
+    fun insertAndReadLatestRates() = runBlocking {
+        val rate = LatestRateEntity(
+            currencyName = "USD",
+            conversionRate = 0.98
+        )
+        currencyDao.insertRate(rate)
+        val latestRate = currencyDao.getLatestRateByKey(rate.currencyName)
+        assertThat(latestRate.conversionRate).isEqualTo(rate.conversionRate)
+    }
+
+    @Test
+    fun addUserCurrencyValue() = runBlocking {
+        val currency = CurrencyEntity(
+            currencyName = "EUR",
+            currencyBalance = 1000.0
+        )
+        currencyDao.insert(currency)
+        currencyDao.updateSum(currency.currencyName, 15.0)
+
+        val userCurrency = currencyDao.getCurrencyByKey(currency.currencyName)
+        assertThat(userCurrency.currencyBalance).isEqualTo(1015.0)
+    }
+
+    @Test
+    fun minusUserCurrencyValue() = runBlocking {
+        val currency = CurrencyEntity(
+            currencyName = "EUR",
+            currencyBalance = 1000.0
+        )
+        currencyDao.insert(currency)
+        currencyDao.updateMinus(currency.currencyName, 15.0)
+
+        val userCurrency = currencyDao.getCurrencyByKey(currency.currencyName)
+        assertThat(userCurrency.currencyBalance).isEqualTo(985.0)
     }
 
 
